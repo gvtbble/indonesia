ef4085f071f3680c9981b85a5762d87b6c513479
gvtbble <junkeeeeer@gmail.com>
2023-02-20 15:10:12 +0300

    start

--- js/script.js
+++ js/script.js
@@ -55,6 +55,16 @@ ready(() => {
 	});
 });
 
+const search = document.querySelector(".search") 
+const body = document.querySelector(".body")
+
+search.addEventListener('click' , function (e) {
+	e.stopPropagation()
+	this.classList.add('search-active');
+});
+body.addEventListener('click' , function (e) {
+	search.classList.remove('search-active');
+});
 
 /*
 
